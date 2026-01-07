ad85db4ec616518970963c9db8dc9ca039b54b29
Erick Pintor <erickpintor@gmail.com>
2026-01-07 10:22:33 -0300

    test(mbt): Rename expected_proposal to proposal

--- tests/mbt/src/driver/handle_get_decided.rs
+++ tests/mbt/src/driver/handle_get_decided.rs
@@ -5,12 +5,7 @@ use crate::driver::{process_app_message, EmeraldDriver};
 use crate::state::{Height, Node, Proposal};
 
 impl EmeraldDriver {
-    pub fn handle_get_decided(
-        &mut self,
-        node: Node,
-        height: Height,
-        expected_proposal: Option<Proposal>,
-    ) {
+    pub fn handle_get_decided(&mut self, node: Node, height: Height, proposal: Option<Proposal>) {
         let app = self.nodes.get_mut(&node).expect("Unknown node");
         let (reply_tx, reply_rx) = tokio::sync::oneshot::channel();
 
@@ -22,7 +17,7 @@ impl EmeraldDriver {
         self.runtime.block_on(async {
             process_app_message(app, msg).await;
 
-            let proposal = reply_rx
+            let emerald_proposal = reply_rx
                 .await
                 .expect("Failed to handle GetDecidedValue")
                 .and_then(|raw_decided| {
@@ -30,7 +25,7 @@ impl EmeraldDriver {
                     self.proposals.get_by_right(&value_id)
                 });
 
-            assert_eq!(expected_proposal.as_ref(), proposal);
+            assert_eq!(proposal.as_ref(), emerald_proposal);
         });
     }
 }
--- tests/mbt/src/driver/handle_get_value.rs
+++ tests/mbt/src/driver/handle_get_value.rs
@@ -12,7 +12,7 @@ impl EmeraldDriver {
         node: Node,
         height: Height,
         round: Round,
-        expected_proposal: Proposal,
+        proposal: Proposal,
     ) {
         let app = self.nodes.get_mut(&node).expect("Unknown node");
         let (reply_tx, reply_rx) = tokio::sync::oneshot::channel();
@@ -27,24 +27,24 @@ impl EmeraldDriver {
         self.runtime.block_on(async {
             process_app_message(app, msg).await;
 
-            let proposal = reply_rx.await.expect("Failed to handle GetValue");
-            self.values
-                .insert(expected_proposal.id(), proposal.value.clone());
+            let emerald_proposal = reply_rx.await.expect("Failed to handle GetValue");
+            let emerald_value = emerald_proposal.value.clone();
+            self.values.insert(proposal.id(), emerald_value);
 
-            let value_id = proposal.value.id();
+            let value_id = emerald_proposal.value.id();
             let bytes = app
                 .state
-                .get_block_data(proposal.height, proposal.round, value_id)
+                .get_block_data(emerald_proposal.height, emerald_proposal.round, value_id)
                 .await
                 .expect("Block data must be stored after GetValue");
 
             let stream_msgs = app
                 .state
-                .stream_proposal(proposal, bytes, EmeraldRound::Nil)
+                .stream_proposal(emerald_proposal, bytes, EmeraldRound::Nil)
                 .collect();
 
-            self.streams.insert(expected_proposal.id(), stream_msgs);
-            self.proposals.insert(expected_proposal, value_id);
+            self.streams.insert(proposal.id(), stream_msgs);
+            self.proposals.insert(proposal, value_id);
         });
     }
 }

026371ffc3a2cf8f43ab6b7d85239374f0864e53
Erick Pintor <erickpintor@gmail.com>
2026-01-15 11:07:38 -0300

    test(mbt): Document lib.rs

--- tests/mbt/src/lib.rs
+++ tests/mbt/src/lib.rs
@@ -7,7 +7,10 @@ mod sut;
 #[cfg(test)]
 mod tests;
 
+// NOTE: We export the primary MBT struct so that Rust can consider any code not
+// referenced by the test driver as dead code.
 pub use driver::EmeraldDriver;
 
-// Must match spec's node identifiers.
+// Node identifiers. They must match the `emerald_mbt.qnt` and
+// `emerald_tests.qnt` specifications.
 const NODES: [&str; 3] = ["node1", "node2", "node3"];

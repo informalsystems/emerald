f1113099dfd7b1bbfccc4aad27453e726db14e66
Erick Pintor <erickpintor@gmail.com>
2026-01-16 11:07:07 -0300

    test(mbt): Document sut.rs

--- tests/mbt/src/sut.rs
+++ tests/mbt/src/sut.rs
@@ -16,13 +16,39 @@ use tokio::sync::oneshot::Receiver;
 
 use crate::runtime::Runtime;
 
+/// The Emerald system under test (SUT).
+///
+/// There is one instance of [Sut] per Emerald node in the Quint trace, which is
+/// composed of its address, state components, and local runtime.
+///
+/// Note that SUT implementation is split on different files at the `sut/`
+/// directory where each file focus on the translation of one Quint action.
 pub struct Sut {
     pub address: Address,
     pub components: StateComponents,
+    // XXX: There is some task being spawned into the current tokio runtime
+    // during the StateComponents initialization that is leaking in the sense
+    // that dropping the StateComponents struct doesn't cancel the loop task.
+    //
+    // This comes up in the form of a held lock on Malachite's WAL that prevents
+    // us from simulating a node restart by simply dropping the struct and
+    // recreating it pointing to the same home dir.
+    //
+    // Adding a separate runtime per SUT is a workaround so that we can drop
+    // both StateComponents and its associated Runtime. Dropping the Tokio
+    // runtime also drops the loop tasks that are holding the WAL lock, hence
+    // allowing us to simulate process restarts where the same home dir is
+    // reused.
     pub runtime: Runtime,
 }
 
 impl Sut {
+    /// Process an [AppMsg] using Emerald consensus logic.
+    ///
+    /// This is the primary Emerald entrypoint for testing. For every Quint
+    /// action that needs to be replayed into the Emerald instance, the test
+    /// code must translate the Quint step into an [AppMsg] and feed it to this
+    /// method.
     async fn process_msg<Out>(
         &mut self,
         msg: AppMsg<EmeraldContext>,
